37e40013d44b81eabc044ac24f482ce17e136498
Rohan Singh <rohansingh9001>
2020-01-13 04:18:30 +0530

    Cleaned up code

--- newApp/collegeView.py
+++ newApp/collegeView.py
@@ -1,15 +1,16 @@
 from django.shortcuts import redirect, render
-from django.views.generic import TemplateView
-from django.views.generic import CreateView,ListView
+from django.views.generic import CreateView, ListView
 from .models import User
 from .forms import CollegeSignupForm
 from django.contrib.auth import login
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import View,UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import UpdateView
 from django.urls import reverse
 
+
 def profile(request):
-    return render(request,'newApp/college/profile.html')
+    return render(request, 'newApp/college/profile.html')
+
 
 class SignupView(CreateView):
     model = User
@@ -23,22 +24,40 @@ class SignupView(CreateView):
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('home')    
+        return redirect('home')
+
 
 class PendingQueryView(ListView):
-   def get_queryset(self):
-        return User.objects.filter(Verified=False,is_alumni=True,College=self.request.user.username)    
-   template_name = "pendingalumni.html"
-   context_object_name = 'alumnis'
-   ordering = ['Year_Joined']
-   paginate_by = 12
-        
-class AlumniAuthenticationView(LoginRequiredMixin,UpdateView):
+    def get_queryset(self):
+        return User.objects.filter(
+            Verified=False,
+            is_alumni=True,
+            College=self.request.user.username)
+    template_name = "pendingalumni.html"
+    context_object_name = 'alumnis'
+    ordering = ['Year_Joined']
+    paginate_by = 12
+
+
+class AlumniAuthenticationView(LoginRequiredMixin, UpdateView):
     model = User
-    fields = ["username","first_name","last_name","email","College","About","Work","Year_Joined","Branch","Image","Verified"]
+    fields = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "College",
+        "About",
+        "Work",
+        "Year_Joined",
+        "Branch",
+        "Image",
+        "Verified"
+        ]
     template_name = 'verify.html'
 
-    def form_valid(self,form):
-        return super().form_valid(form) 
+    def form_valid(self, form):
+        return super().form_valid(form)
+
     def get_success_url(self):
-            return reverse('pending-query')   
+        return reverse('pending-query')
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 import webbrowser
-import time
+
 webbrowser.open("http://127.0.0.1:8000/")
